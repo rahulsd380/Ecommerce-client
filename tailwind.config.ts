13777ab3292906182a0d2630c41776e89272e175
Rahul Sutradhar <rahulsd380@gmail.com>
2024-12-02 10:14:37 +0600

    Project setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,21 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily : {
+        Inter : ["Inter", "sans-serif"],      // for body font
+        Poppins : ["Poppins", "sans-serif"],  //for heading
+        Sora : ["Sora", "sans-serif"],        // for headins
+      },
+
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary : {
+          10 : "#29A56C",
+
+        },
+        neutral : {
+          10 : "#253D4E",  // for heading
+          20 : "#4A4A4A"   // for body text
+        },
       },
     },
   },
